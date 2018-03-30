d849b59793d5aaee42e90d8a969fa84ee76aad67
ChocOZerO <nayalth@gmail.com>
2018-03-30 18:51:49 +0900

    [Init]WebViewApp #1

---
+++ WebViewApp/WebViewApp/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  WebViewApp
+//
+//  Created by TaeHyeonLee on 2018. 3. 30..
+//  Copyright © 2018년 ChocOZerO. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, UIWebViewDelegate {
+
+    @IBOutlet weak var webView: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        guard let url = URL(string: "https://m.baeminchan.com") else { return }
+        webView.loadRequest(URLRequest(url: url))
+        webView.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        print(#function)
+        print(webView, request, navigationType)
+        guard request.url != nil else { return false }
+        return true
+    }
+
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        print(#function)
+        print(webView)
+    }
+
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        print(#function)
+        print(webView)
+    }
+
+    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+        print(#function)
+        print(webView, error)
+    }
+
+}
+
