62904e2fe888cb1e8239a622f6af977d0c95c172
ChocOZerO <nayalth@gmail.com>
2018-04-02 18:38:15 +0900

    [Update]모든 팝업 감추기 #8

--- WebViewApp/WebViewApp/Injection.js
+++ WebViewApp/WebViewApp/Injection.js
@@ -1,4 +1,8 @@
-var popup = document.querySelector('.app-download-popup');
-if (popup != null) {
-    popup.style.display = 'none';
+var main_popup = document.querySelector('.app-download-popup');
+if (main_popup != null) {
+    main_popup.style.display = 'none';
+}
+var event_popup = document.querySelector('.mobile_wm_popup');
+if (event_popup != null) {
+    event_popup.style.display = 'none';
 }
