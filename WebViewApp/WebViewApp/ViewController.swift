be967b9edf22c86af13a8a600acb7dbd17f55d8d
ChocOZerO <nayalth@gmail.com>
2018-04-02 17:26:19 +0900

    [Update]팝업창 제거 #5

--- WebViewApp/WebViewApp/ViewController.swift
+++ WebViewApp/WebViewApp/ViewController.swift
@@ -9,18 +9,36 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
 
-    @IBOutlet weak var webView: WKWebView!
+    private weak var webView: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        setWebView()
         guard let url = URL(string: "https://m.baeminchan.com") else { return }
-        webView.load(URLRequest(url: url))
+        let urlRequest = URLRequest(url: url)
+        self.webView.load(urlRequest)
+    }
+
+    private func setWebView() {
+        let webConfiguration = WKWebViewConfiguration()
+        let contentController = WKUserContentController()
+        let source = """
+            var popup = document.querySelector('.app-download-popup');
+            if (popup != null) {
+                popup.style.display = 'none';
+            }
+            console.log("ttttt")
+        """
+        let userScript = WKUserScript(source: source, injectionTime: .atDocumentEnd, forMainFrameOnly: false)
+        contentController.addUserScript(userScript)
+        webConfiguration.userContentController = contentController
+        webView = WKWebView(frame: view.frame, configuration: webConfiguration)
+        view.addSubview(webView)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 }
