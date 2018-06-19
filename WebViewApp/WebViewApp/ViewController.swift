c2ce535f259293775fab3fdb518f1ea40554be50
Mijin Jeon <mayihelpu27@gmail.com>
2018-06-19 15:26:03 +0900

    Chore: Update .gitignore and add project files

---
+++ WebViewApp/WebViewApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WebViewApp
+//
+//  Created by YOUTH2 on 2018. 6. 19..
+//  Copyright © 2018년 JINiOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
