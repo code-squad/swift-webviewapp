a9e34da626bd6830d2720d03b2d79987d1609dc0
Mijin Jeon <mayihelpu27@gmail.com>
2018-06-25 20:09:55 +0900

    Feat: Implement WKScriptMessageHandler #3 - And Modify injectionsource.js to get content in .silde.navi

--- WebViewApp/WebViewApp/injectionsource.js
+++ WebViewApp/WebViewApp/injectionsource.js
@@ -2,3 +2,20 @@ var popup = document.querySelector('.app-download-popup');
 if (popup != null) {
     popup.style.display = 'none';
 }
+
+function callNative() {
+    var slideNavi = document.querySelectorAll('ul.slide-navi > li > a');
+    pos = []
+    if (slideNavi != null) {
+        for (var i = 0; i < slideNavi.length; i++) {
+            var value = slideNavi[i];
+            var valueTitle = value.textContent;
+            var valueURL = value.getAttribute('href');
+            pos.push({'valueTitle': valueTitle, 'valueURL': valueURL});
+        }
+        webkit.messageHandlers.slideMenu.postMessage(pos);
+    }
+}
+
+callNative();
+
