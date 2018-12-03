d89bbc47068c367a56af8caddf1219964cf8d5ed
oingbong <oingbong@gmail.com>
2018-12-03 17:40:59 +0900

    init UIWebview

--- WebviewApp/WebviewApp/ViewController.swift
+++ WebviewApp/WebviewApp/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var webview: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        webview.delegate = self
+        guard let url = URL(string: "https://m.baeminchan.com") else { return }
+        let request = URLRequest(url: url)
+        webview.loadRequest(request)
+        self.view.addSubview(webview)
     }
-
-
 }
 
+extension ViewController: UIWebViewDelegate {
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        print(#function)
+        print("webView : \(webView)")
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        print(#function)
+        print("webView : \(webView)")
+    }
+    
+    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+        print(#function)
+        print("webView : \(webView)")
+        print("error : \(error)")
+    }
+}
