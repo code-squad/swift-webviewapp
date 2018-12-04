19bfb925754c24e9c94424da7487e8381b62a1d2
oingbong <oingbong@gmail.com>
2018-12-04 14:27:01 +0900

    Add script source for delete popup

--- WebviewApp/WebviewApp/ViewController.swift
+++ WebviewApp/WebviewApp/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 import WebKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var webview: WKWebView!
+    var webview: WKWebView!
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        let source = """
+        var popup = document.querySelector('.app-download-popup');
+        if (popup != null) {
+            popup.style.display = 'none';
+        }
+        """
+        let userScript = WKUserScript(source: source, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+        let contentController = WKUserContentController()
+        contentController.addUserScript(userScript)
+        webConfiguration.userContentController = contentController
+        webview = WKWebView(frame: .zero, configuration: webConfiguration)
+        view = webview
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         guard let url = URL(string: "https://m.baeminchan.com") else { return }
         let request = URLRequest(url: url)
         webview.load(request)
