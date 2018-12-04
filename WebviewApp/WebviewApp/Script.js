7612d63650a083f33b5be20195481bd4d68c9a8d
oingbong <oingbong@gmail.com>
2018-12-04 20:46:22 +0900

    Edit when click menubar, print menuList in swift

--- WebviewApp/WebviewApp/Script.js
+++ WebviewApp/WebviewApp/Script.js
@@ -12,11 +12,25 @@ menubar.addEventListener('click', function(){ scriptHandler("menuBar"); });
 function scriptHandler(message) {
     if (message == "searchBar") {
         event.preventDefault();
+        
+        try {
+            webkit.messageHandlers.scriptHandler.postMessage(message);
+        } catch(error) {
+            alert(error);
+        }
     }
     
-    try {
-        webkit.messageHandlers.scriptHandler.postMessage(message);
-    } catch(error) {
-        alert(error);
+    if (message == "menuBar") {
+        var menu = document.querySelectorAll('.slide-navi > li > a');
+        var data = [];
+        for (var i = 0; i < menu.length; i++) {
+            data.push({'title': menu[i].innerHTML, 'url': menu[i].getAttribute('href')});
+        }
+        console.log(data);
+        try {
+            webkit.messageHandlers.scriptHandler.postMessage(data);
+        } catch(error) {
+            alert(error);
+        }
     }
 }
