1c843d5f50d3901de0c3e48a122853ccd0a0ca95
oingbong <oingbong@gmail.com>
2018-12-04 16:15:51 +0900

    Add WKUIDelegate for alert in WKWebview & Divide js file and read Bundle path

--- WebviewApp/WebviewApp/ViewController.swift
+++ WebviewApp/WebviewApp/ViewController.swift
@@ -14,8 +14,17 @@ class ViewController: UIViewController {
     override func loadView() {
         super.loadView()
         
+        var content = ""
+        if let path = Bundle.main.path(forResource: "Script", ofType: "js") {
+            do {
+                content = try String(contentsOfFile: path)
+            } catch {
+                content = Key.alert(with: "에러가 발생하였습니다.")
+            }
+        }
+        
         let webConfiguration = WKWebViewConfiguration()
-        let userScript = WKUserScript(source: Script.popupDisable, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+        let userScript = WKUserScript(source: content, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
         let contentController = WKUserContentController()
         contentController.addUserScript(userScript)
         webConfiguration.userContentController = contentController
@@ -25,8 +34,22 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        guard let url = URL(string: "https://m.baeminchan.com") else { return }
+        webview.uiDelegate = self
+        guard let url = URL(string: Key.url) else { return }
         let request = URLRequest(url: url)
         webview.load(request)
+    }
+}
+
+extension ViewController: WKUIDelegate {
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
+        let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: Key.alertTitle, style: .cancel) { _ in
+            completionHandler()
+        }
+        alertController.addAction(cancelAction)
+        DispatchQueue.main.async {
+            self.present(alertController, animated: true, completion: nil)
+        }
     }
 }
