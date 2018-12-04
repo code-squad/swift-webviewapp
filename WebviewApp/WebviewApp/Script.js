95f3077d9ab0d5ad7c124b17ed49c93a9799c1aa
oingbong <oingbong@gmail.com>
2018-12-04 19:08:56 +0900

    Add prevent event at search Button

--- WebviewApp/WebviewApp/Script.js
+++ WebviewApp/WebviewApp/Script.js
@@ -2,3 +2,10 @@ var popup = document.querySelector('.app-download-popup');
 if (popup != null) {
     popup.style.display = 'none';
 }
+
+var search = document.querySelector('.btn-search');
+search.addEventListener('click', changeSearch );
+
+function changeSearch() {
+    event.preventDefault();
+}
