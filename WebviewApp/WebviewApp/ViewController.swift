ba80de5db04d23cd2f62be15987dc0449656afc2
hngfu <djsl1018@naver.com>
2019-04-09 17:48:26 +0900

    [ViewController] UIWebViewDelegate 채택 및 준수

--- WebviewApp/WebviewApp/ViewController.swift
+++ WebviewApp/WebviewApp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var uiWebView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,3 +20,25 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UIWebViewDelegate {
+    
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        print(#function)
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        print(#function)
+    }
+    
+    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+        print(#function)
+        print("error", error.localizedDescription)
+    }
+    
+    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebView.NavigationType) -> Bool {
+        print(#function)
+        print("request", request)
+        print("navigationType", navigationType)
+        return false
+    }
+}
