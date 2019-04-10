fe12158778f63b5b335ff5870ec2d5db1d335da3
hngfu <djsl1018@naver.com>
2019-04-10 15:45:39 +0900

    [ViewController] WebView load관련 내용 구현

--- WebviewApp/WebviewApp/ViewController.swift
+++ WebviewApp/WebviewApp/ViewController.swift
@@ -7,15 +7,24 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var wkWebView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let stringURL = "https://m.shopping.naver.com"
+        guard let url = URL(string: stringURL) else { return }
+        let urlRequest = URLRequest(url: url)
+        wkWebView.load(urlRequest)
     }
 
 
 }
 
+extension ViewController: WKUIDelegate {
+    
+}
