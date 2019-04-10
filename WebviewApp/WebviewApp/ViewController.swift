cb5954a1739eb8ff20502ff537fc25ce583a9df0
hngfu <djsl1018@naver.com>
2019-04-10 14:51:02 +0900

    [ViewController] UIWebView관련 내용 삭제, [main.storyboard] UIWebView대신 WKWebView 추가

--- WebviewApp/WebviewApp/ViewController.swift
+++ WebviewApp/WebviewApp/ViewController.swift
@@ -9,40 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var uiWebView: UIWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let stringURL = "https://m.shopping.naver.com"
-        guard let url = URL(string: stringURL) else { return }
-        let urlRequest = URLRequest(url: url)
-        uiWebView.loadRequest(urlRequest)
+
         // Do any additional setup after loading the view.
     }
 
 
 }
 
-extension ViewController: UIWebViewDelegate {
-    
-    func webViewDidStartLoad(_ webView: UIWebView) {
-        print(#function)
-    }
-    
-    func webViewDidFinishLoad(_ webView: UIWebView) {
-        print(#function)
-    }
-    
-    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
-        print(#function)
-        print("error", error.localizedDescription)
-    }
-    
-    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebView.NavigationType) -> Bool {
-        print(#function)
-        print("request", request)
-        print("navigationType", navigationType)
-        return false
-    }
-}
