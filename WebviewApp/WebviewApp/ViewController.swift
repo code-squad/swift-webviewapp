f09e21db8998c884bcf4e67611bdce0c07412f2d
hngfu <djsl1018@naver.com>
2019-04-17 21:46:52 +0900

    [ViewController] WKNavigationDelegate 채택, webView(_:decidePolicyFor:decisionHandler) 구현

--- WebviewApp/WebviewApp/ViewController.swift
+++ WebviewApp/WebviewApp/ViewController.swift
@@ -38,10 +38,25 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.wkWebView?.navigationDelegate = self
         let stringURL = "https://m.shopping.naver.com"
         guard let url = URL(string: stringURL) else { return }
         let urlRequest = URLRequest(url: url)
         wkWebView?.load(urlRequest)
+    }
+}
+
+extension ViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let path = "/menu.nhn"
+        
+        if navigationAction.navigationType == .linkActivated,
+            let requestURL = navigationAction.request.url,
+            requestURL.absoluteString.contains(path) {
+            
+            decisionHandler(.cancel)
+        } else {
+            decisionHandler(.allow)
+        }
     }
 }
