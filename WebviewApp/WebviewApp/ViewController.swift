343119f710b55663f26148c46151b767cc89fa6d
kiljoonho <kiljh@me.com>
2019-08-27 15:44:26 +0900

    웹뷰를 제거하고 WKWebView 구현

--- WebviewApp/WebviewApp/ViewController.swift
+++ WebviewApp/WebviewApp/ViewController.swift
@@ -7,39 +7,16 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController, UIWebViewDelegate {
-    @IBOutlet weak var webView: UIWebView!
+class ViewController: UIViewController {
+    @IBOutlet weak var webView: WKWebView!
     let url = URL(string: "https://m.shopping.naver.com")
         
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        webView.delegate = self
         
-        if let url = url{
-            webView.loadRequest(URLRequest(url: url))
-        }
-    }
-    
-    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebView.NavigationType) -> Bool {
-        print(#function)
-        dump(request)
-        dump(navigationType)
-        return true
-    }
-    
-    func webViewDidStartLoad(_ webView: UIWebView) {
-        print(#function)
-    }
-    
-    func webViewDidFinishLoad(_ webView: UIWebView) {
-        print(#function)
-    }
-    
-    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
-        print(#function)
-        dump(error)
     }
 }
 
