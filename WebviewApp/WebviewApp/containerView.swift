2d29377dd4a56347c468d4e5a723b2d1c9d75168
kiljoonho <kiljh@me.com>
2019-08-28 16:46:00 +0900

    WKUserScript 와 evaluateJavaScript 을 이용해 자바스크립트를 실행시켜서 part_banner 제거

--- WebviewApp/WebviewApp/containerView.swift
+++ WebviewApp/WebviewApp/containerView.swift
@@ -7,21 +7,34 @@
 //
 
 import UIKit
+import WebKit
 
-class containerView: UIView {
+class containerView: UIView, WKNavigationDelegate {
     override func draw(_ rect: CGRect) {
-        let source = "var popup = document.querySelector('.part_banner'); if (popup != null) { popup.style.display = 'none'; }"
-        let userScript = WKUserScript.init(source: source, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
-        
-        let contentController = WKUserContentController.init()
-        contentController.addUserScript(userScript)
+        let contentController = WKUserContentController()
+        let scriptSource = "var popup = document.querySelector(`.part_banner`); if (popup != null) { popup.style.display = `none`; }"
+        let script = WKUserScript(source: scriptSource, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+        contentController.addUserScript(script)
         
         let config = WKWebViewConfiguration()
         config.userContentController = contentController
-        self.webView = WKWebView(frame: webView.bounds, configuration: config)
         
+        let webView = WKWebView(frame: self.bounds, configuration: config)
+        
+        let url = URL(string: "https://m.shopping.naver.com")
         let request = URLRequest(url: url!)
         webView.load(request)
+        addSubview(webView)
+        webView.navigationDelegate = self
     }
-
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        let scriptSource = "var popup = document.querySelector(`.part_banner`); if (popup != null) { popup.style.display = `none`; }"
+        
+        webView.evaluateJavaScript(scriptSource, completionHandler: { (object, error) in
+            
+        })
+    }
 }
+
+
