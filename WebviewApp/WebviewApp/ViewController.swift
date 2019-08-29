468b3e171079bcfedbaf16de129cdfa5e2724b44
kiljoonho <kiljh@me.com>
2019-08-29 21:06:31 +0900

    원하는 URL 로 웹뷰가 띄어질 수 있도록 메소드 실행

--- WebviewApp/WebviewApp/ViewController.swift
+++ WebviewApp/WebviewApp/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var containerView: ContainerView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        containerView.setupView()
+        containerView.webViewLoad(url: URL(string: "https://m.shopping.naver.com")!)
     }
 }
 
