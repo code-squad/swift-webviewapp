53b9e13bc01dc8aef3392a61bc0656343f31ae00
부엉이 <o.o.wl.soft@gmail.com>
2019-11-30 02:04:48 +0900

    chore: gitignore 에서 /Pods 적용되게 수정

---
+++ WebviewApp/WebviewApp/Common/Constants/Naver.swift
@@ -0,0 +1,9 @@
+//
+//  Naver.swift
+//  WebviewApp
+//
+//  Created by 이동영 on 2019/11/29.
+//  Copyright © 2019 이동영. All rights reserved.
+//
+
+import Foundation
---
+++ WebviewApp/WebviewApp/View/MyUIWebView.swift
@@ -0,0 +1,9 @@
+//
+//  MyUIWebView.swift
+//  WebviewApp
+//
+//  Created by 이동영 on 2019/11/30.
+//  Copyright © 2019 이동영. All rights reserved.
+//
+
+import Foundation
